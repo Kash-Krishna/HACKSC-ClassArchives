c517259cc6dc60e90018b63ede67bf0683bc9577
Kash Krishna <Kashys5@gmail.com>
2014-11-08 12:24:32 -0800

    Adding website stuff
    change 1 with many errors

---
+++ classarchive1/loggedIn.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>New Piazza</title>
+    </head>
+    <body>
+            <table>
+               <tr>
+		            <td class="menu" align="center"><a href="loggedIn.html">SUBMIT BUTTON COMES HERE!</a></td>
+	           </tr>
+            </table>
+    </body>
+</html>
---
+++ classarchive1/stdnt/hostingstart.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>New Piazza</title>
+        <head>
+            <link rel="stylesheet" type="text/css" href="main.css">
+        </head>
+	</head>
+	<body>
+		<form action="welcome.php" method="post">
+            Name: <input type="text" name="name"><br>
+            Password: <input type="text" name="email"><br>
+            <input type="submit">
+         </form>			
+	</body>
+</html>
